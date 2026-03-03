e19661d731b6c64833b44c0ae2dcc98685acdfce
kuhcokrow <kuhhhckr@gmail.com>
2026-03-03 22:20:27 +0700

    feat: remove pages doc and example

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { ProtectedRoute } from './components/ProtectedRoute'
 import { Home } from './pages/Home'
 import { Dashboard } from './pages/Dashboard'
 import { KYCStatus } from './pages/KYCStatus'
-import { Docs } from './pages/Docs'
-import { Examples } from './pages/Examples'
 
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/kyc-status" element={<ProtectedRoute><KYCStatus /></ProtectedRoute>} />
           <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/docs" element={<Docs />} />
-          <Route path="/examples" element={<Examples />} />
         </Routes>
       </Layout>
     </Router>

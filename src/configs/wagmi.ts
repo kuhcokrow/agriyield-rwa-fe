cf39fd3155c9550b6f17c09d1ef07e549738dfc6
Kukuh Cokro Wibowo <92429096+kuhcokrow@users.noreply.github.com>
2026-03-13 19:01:06 +0700

    fix: connect button and regenerate pnpm lockfile (#7)
    * chore: change title
    
    * refactor: from template to projects rwa
    
    * feat: KYC status
    
    * feat: install shadcn
    
    * refactor: ui tone theme
    
    * feat: remove pages doc and example
    
    * feat: update button component
    
    * feat: workflows ci/cd
    
    * refactor: devide variants
    
    * fix: import icon
    
    * feat: update workflows ci/cd
    
    * feat: update dependencies
    
    * feat: update vercel deploy workflows
    
    * fix: workflows build vercel
    
    * fix: workflows jobs
    
    * fix(ui): change styling by props
    
    * fix(issue): default chain config
    
    * fix(ui): nav header transpant
    
    * feat: admin owner protector
    
    * chore: add variable and module
    
    * feat: update dependencies
    
    * fix(issue): config rendering
    
    * fix: dependencies issue
    
    * fix: ci/cd
    
    * fix: ci/cd staging
    
    * fix: connect button
    
    * fix: regenerate pnpm lockfile

--- src/configs/wagmi.ts
+++ src/configs/wagmi.ts
@@ -1,13 +1,10 @@
 import { createConfig, http } from 'wagmi'
 import { mantleSepolia } from './chains'
-import { injected, walletConnect } from 'wagmi/connectors'
+import { injected } from 'wagmi/connectors'
 
 export const config = createConfig({
   chains: [mantleSepolia],
-  connectors: [
-    injected(),
-    walletConnect({ projectId: 'YOUR_PROJECT_ID' }),
-  ],
+  connectors: [injected()],
   transports: {
     [mantleSepolia.id]: http('https://rpc.sepolia.mantle.xyz'),
   },
